89694be33deeb556c7e3607486544759889bbf58
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-06-30 20:01:07 -0700

    season 10

--- src/components/stuffThatChangesEverySeason.js
+++ src/components/stuffThatChangesEverySeason.js
@@ -1,7 +1,7 @@
-export const currentSeasonNumber = 9;
+export const currentSeasonNumber = 10;
 export const currentSeasonOrdinal = "th";
 
-export const contractAddress = "0xb40d231FA012a171deA90Af7b432F79Db18ef4B2";
-export const oldContractAddress = "0xd8544E4d184b6d7bae36999897298bC9692b871b";
+export const contractAddress = "0x83135493E3f15a7Bf2047DAF7288ffB3f0d4a28A";
+export const oldContractAddress = "0xb40d231FA012a171deA90Af7b432F79Db18ef4B2";
 
-export const openseaURL = "//opensea.io/collection/jpegmp9";
+export const openseaURL = "//opensea.io/collection/jpegmp10";
